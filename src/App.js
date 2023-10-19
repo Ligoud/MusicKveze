82637f77ec3c5fef790ff428ea10ee36bdec65fd
Yegor <ligoudnd@gmail.com>
2023-10-20 02:28:03 +0300

    База сделана

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useCallback, useEffect, useMemo, useState } from "react";
+import socket from "./logic/socket";
+const sock = new socket();
 function App() {
+  //
+  const [regObj, setRegObj] = useState({
+    alert: "",
+    isOk: false,
+  });
+  //
+
+  const [name, setName] = useState("");
+  const [isBlocked, setIsBlocked] = useState(false);
+  const [asnwerUser, setAnswerUser] = useState("");
+  const [score, setScore] = useState({});
+
+  // callbacks
+  const nameChange = useCallback((ev) => {
+    setName(ev.target.value);
+  }, []);
+  const onRegister = useCallback(() => {
+    sock.register(name);
+  }, [name]);
+  const onAnswer = useCallback(() => {
+    sock.answer(name);
+  }, [name]);
+  const setAnswer = useCallback((isBlck) => {
+    sock.setBlock(isBlck);
+  }, []);
+  const deleteUsers = useCallback((isBlck) => {
+    sock.deleteAll();
+  }, []);
+  const addScore = useCallback((sc) => {
+    sock.addScore(sc);
+  }, []);
+  //
+  const displayLogin = useMemo(() => {
+    return !regObj.alert || (!!regObj.alert && !regObj.isOk);
+  }, [regObj]);
+
+  const displayScore = useMemo(() => {
+    const disp = [];
+
+    for (let sc in score) {
+      if (sc !== "admin")
+        disp.push(
+          <>
+            <div key={sc}>
+              {sc}: {score[sc]}
+            </div>
+          </>
+        );
+    }
+    return disp;
+  }, [score]);
+  //
+  useEffect(() => {
+    sock.init({
+      setMsg: setRegObj,
+      setBlocked: setIsBlocked,
+      setAnswerUser,
+      setScore,
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {displayLogin && (
+        <>
+          <input onChange={nameChange} value={name} />
+          <button onClick={onRegister}>Войти</button>
+          <br />
+          {regObj.alert}
+        </>
+      )}
+      {!displayLogin && (
+        <>
+          {name}
+          <br />
+          {name === "admin" ? (
+            <>
+              Отвечает: {asnwerUser || " - "}
+              {asnwerUser && (
+                <>
+                  <button onClick={() => addScore(1)}>Верно</button>
+                  <button onClick={() => addScore(0)}>Неверно</button>
+                </>
+              )}
+              <br />
+              Пользователи: {isBlocked ? "Заблокированы" : "Разблокированы"}
+              <br />
+              <button onClick={() => setAnswer(true)}>Заблокировать</button>
+              <br />
+              <button onClick={() => setAnswer(false)}>Разблокировать</button>
+              <br />
+              <button onDoubleClick={deleteUsers}>Обнулить игроков</button>
+              <br />
+              <br />
+              Участники:
+              <br />
+              {displayScore}
+            </>
+          ) : (
+            <button onClick={onAnswer} disabled={isBlocked}>
+              Ответить
+            </button>
+          )}
+        </>
+      )}
     </div>
   );
 }
---
+++ src/logic/socket.js
@@ -0,0 +1,50 @@
+const connection = new WebSocket("ws://localhost:8080");
+connection.onopen = () => {
+  console.log("connected");
+};
+
+export default class OwnSocket {
+  init({ setMsg, setBlocked, setAnswerUser, setScore }) {
+    connection.onmessage = ({ data }) => {
+      const resData = JSON.parse(data);
+
+      switch (resData.type) {
+        case "login":
+          setMsg({
+            alert: resData.alert,
+            isOk: resData.isOk,
+          });
+          break;
+        case "answer":
+          setBlocked(resData.isBlocked);
+          setAnswerUser(resData.answerUser);
+          break;
+        case "score":
+          setScore(resData.score);
+          setAnswerUser(resData.answerUser);
+          break;
+        default:
+          break;
+      }
+    };
+  }
+  register(name) {
+    connection.send(JSON.stringify({ type: "login", name }));
+    //
+  }
+
+  answer(name) {
+    connection.send(JSON.stringify({ type: "answer", name }));
+  }
+
+  setBlock(isBlocked) {
+    connection.send(JSON.stringify({ type: "setblock", isBlocked }));
+  }
+
+  addScore(score) {
+    connection.send(JSON.stringify({ type: "addscore", score }));
+  }
+  deleteAll() {
+    connection.send(JSON.stringify({ type: "deleteAll" }));
+  }
+}
